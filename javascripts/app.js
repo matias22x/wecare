9020fbdca6998ea8212a23822a64723c2581b092
matias <matias@grava.digital>
2018-05-06 18:04:09 -0300

    modificada url api

--- javascripts/app.js
+++ javascripts/app.js
@@ -9,7 +9,7 @@ var wecareApp = angular.module('wecareApp', ['satellizer', 'ui.router', 'ui.mate
     'angular-web-notification'
 ]);
 wecareApp.constant('config', {
-    api_url: 'http://159.65.176.157:3000/',
+    api_url: 'http://159.65.176.157:3000',
     front_url: '/',
     user_validation: new RegExp('^(([A-z0-9]){4,20})$'),
     pass_validation: new RegExp('^(([A-z0-9@]){8,20})$')
