c75560ab553561327ed8e1e82d0b5da653f78ca7
matias <brbrbr@live.com.ar>
2018-06-16 17:08:13 -0300

    solo falta entrenar y meter todo esto en un recorrido. y algo de contador de gravedad

--- javascripts/controllers/bot-controller.js
+++ javascripts/controllers/bot-controller.js
@@ -4,18 +4,29 @@ angular.module('wecareApp')
     $scope.datos = {};
     $scope.datos.pregunta = "¿Con quien vivis?";//INICIALIZO
     $scope.datos.respuesta = "¿Con quien vivis?";//INICIALIZO
+    $scope.enableInput = false;
     $scope.opciones = [];
     $scope.diagnosticoPrematuro = {
       diagnostico: []
     };
 
-    //falta -> guardar,traer opciones, encadenar, entrenar
+    function guardarEnDiagnostico(datos) {
+      if (datos.input) {
+        datos.respuesta = datos.input;
+        datos.pregunta = datos.pregunta.slice(7);
+      }
+      $scope.diagnosticoPrematuro.diagnostico.push({
+        pregunta: datos.pregunta,
+        respuesta: datos.respuesta
+      });
+    }
+    //falta: entrenar y finalizar recorrido.
     $scope.enviar = function(datos) {
-      console.log(datos);
+      $scope.enableInput = false;
       $scope.opciones = [];
-
       botService.postPreguntas(datos)
         .then(function(resp) {
+          $scope.datos.preguntaParseada = '';
           $scope.entities = resp.data.data.entities;
           var arrayEntities = Object.keys($scope.entities).map(function(key) {
               return $scope.entities[key];
@@ -23,24 +34,26 @@ angular.module('wecareApp')
           ).map(function(array) {
             return array[0];
           });
-
           if (arrayEntities.length > 1) {
             Object.keys(arrayEntities).forEach(function(key) {
               $scope.opciones.push(arrayEntities[key].value);
             });
 
-          } else if(arrayEntities.length === 1 || arrayEntities[0].value.indexOf('*INPUT*') !== -1) {
+          } else if(arrayEntities.length === 1 && arrayEntities[0].value.indexOf('*INPUT*') === -1) {
             $scope.datos.pregunta = arrayEntities[0].value;
+            guardarEnDiagnostico(datos);
             $scope.enviar({'respuesta': $scope.datos.pregunta})
 
-          }//recorrido para inputs
+          } else if(arrayEntities[0].value.indexOf('*INPUT*') !== -1) {
+            $scope.enableInput = true;
+            $scope.datos.pregunta = arrayEntities[0].value;
+            $scope.datos.preguntaParseada = arrayEntities[0].value.slice(7);
+            guardarEnDiagnostico(datos);
+          } else if(arrayEntities[0].value.indexOf('Finalizacion del recorrido') !== -1) {
+            //ACA DEBERIA TERMINAR EL CHATBOT.
+          }
         }).catch($log.error);
     }
 
     $scope.enviar($scope.datos);
   });
-  // $scope.diagnosticoPrematuro.diagnostico.push({
-  //   pregunta: $scope.datos.pregunta,
-  //   respuesta: datos,
-  // })
-  // console.log('asi va el diagnostico: ', $scope.diagnosticoPrematuro);
